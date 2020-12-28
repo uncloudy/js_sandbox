5dbfb8e13796d5af27b0a25b16b77da9e2b3c9a5
uncloudy <71613233+uncloudy@users.noreply.github.com>
2020-12-28 21:17:17 +0900

    pre_test

---
+++ app.js
@@ -0,0 +1,22 @@
+// log to console
+console.log("hello");
+console.log(123);
+console.log("true");
+var greeting = "hello";
+console.log(greeting);
+console.log([1, 2, 3]);
+console.log({ a: 1, b: 3 });
+console.table({ a: 1, b: 2 });
+console.error("this is an error");
+console.clear();
+console.warn("this is a warning");
+console.time("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.timeEnd("hello");
+// Multi
+// line
+// comment
